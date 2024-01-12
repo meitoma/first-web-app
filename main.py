c8b694cfd8ac63b51f62b3ec4ac31a96ab1fe57f
meitoma <meitoma0511@gmail.com>
2024-01-13 00:19:20 +0900

    init

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-# test python
+from flask import Flask
+from flask import flash, redirect, url_for,render_template,request
+app = Flask(__name__)
+@app.route('/')
+def index():
+    message = "hello flask"
+    return render_template('index.html', message = message,)
+
+
