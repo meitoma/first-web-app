f0fce6356337cd36c7c69dea8dfca8abdfc92b25
meitoma <meitoma0511@gmail.com>
2024-03-01 15:31:16 +0900

    push通知の記述位置の変更

--- bbs/static/js/login.js
+++ bbs/static/js/login.js
@@ -1,4 +1,64 @@
+document.getElementById('loginButton').addEventListener('click', function() {
+    document.getElementById('loaderContainer').style.display = 'flex';
+    document.getElementById('mask').style.display = 'block';
+    setTimeout(function() {
+        document.getElementById('loaderContainer').style.display = 'none';
+    }, 15000); // 2秒後に非表示にする例
+});
+document.getElementById('signup1').addEventListener('click', function() {
+    document.getElementById("h1_title").innerText = 'サインアップ';
+    signup();
+});
+document.getElementById('signup2').addEventListener('click', function() {
+    signup();
+    document.getElementById("h1_title").innerText = 'サインアップ';
+});
+document.getElementById('signup_cancel').addEventListener('click', function() {
+    login();
+    document.getElementById("h1_title").innerText = 'ログイン';
+});
+
+function login() {
+    // ログインフォーム表示
+    document.getElementById("login-form").style.display = "block";
+    // 新規登録フォーム非表示
+    document.getElementById("signup-form").style.display = "none";
+  }
+  
+function signup() {
+    // 新規登録フォーム表示
+    document.getElementById("signup-form").style.display = "block";
+    // ログインフォーム非表示
+    document.getElementById("login-form").style.display = "none";
+}
+
 var post_currentToken;
+$(function() {
+    $('#loginButton').on('click', function() {
+       $(this).prop('disabled', true);
+       var formData = new FormData(document.getElementById('login-form'));
+       console.log(post_currentToken);
+       formData.append('FCMToken', post_currentToken);
+       const xhr = new XMLHttpRequest();
+       xhr.open('POST', '/login', true);
+       xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                console.log(xhr.responseText);
+                var response = JSON.parse(xhr.responseText);
+                window.location.href = response.redirect_url;
+            }
+        };
+       xhr.send(formData);
+    });
+});
+
+$(function() {
+    $('.one-clk-btn').on('click', function() {
+       $(this).prop('disabled', true);
+       $('#signup-form').submit();
+    });
+});
+
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/bbs/firebase-messaging-sw.js').then(registration => {
         console.log('ServiceWorker registration successful.');
@@ -44,66 +104,6 @@ if ('serviceWorker' in navigator) {
         }
         });
     });
-
-document.getElementById('loginButton').addEventListener('click', function() {
-    document.getElementById('loaderContainer').style.display = 'flex';
-    document.getElementById('mask').style.display = 'block';
-    setTimeout(function() {
-        document.getElementById('loaderContainer').style.display = 'none';
-    }, 15000); // 2秒後に非表示にする例
-});
-document.getElementById('signup1').addEventListener('click', function() {
-    document.getElementById("h1_title").innerText = 'サインアップ';
-    signup();
-});
-document.getElementById('signup2').addEventListener('click', function() {
-    signup();
-    document.getElementById("h1_title").innerText = 'サインアップ';
-});
-document.getElementById('signup_cancel').addEventListener('click', function() {
-    login();
-    document.getElementById("h1_title").innerText = 'ログイン';
-});
-
-function login() {
-    // ログインフォーム表示
-    document.getElementById("login-form").style.display = "block";
-    // 新規登録フォーム非表示
-    document.getElementById("signup-form").style.display = "none";
-  }
-  
-function signup() {
-    // 新規登録フォーム表示
-    document.getElementById("signup-form").style.display = "block";
-    // ログインフォーム非表示
-    document.getElementById("login-form").style.display = "none";
-}
-
-$(function() {
-    $('#loginButton').on('click', function() {
-       $(this).prop('disabled', true);
-       var formData = new FormData(document.getElementById('login-form'));
-       console.log(post_currentToken);
-       formData.append('FCMToken', post_currentToken);
-       const xhr = new XMLHttpRequest();
-       xhr.open('POST', '/login', true);
-       xhr.onreadystatechange = function () {
-            if (xhr.readyState == 4 && xhr.status == 200) {
-                console.log(xhr.responseText);
-                var response = JSON.parse(xhr.responseText);
-                window.location.href = response.redirect_url;
-            }
-        };
-       xhr.send(formData);
-    });
-});
-
-$(function() {
-    $('.one-clk-btn').on('click', function() {
-       $(this).prop('disabled', true);
-       $('#signup-form').submit();
-    });
-});
 
 
 
